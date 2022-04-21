cde54e0b028a50742c5f091a7a9e2e615a14237a
iaacornus <iaacornus@gmail.com>
2022-04-21 16:15:54 +0800

    separated the suggests function

--- src/functions.py
+++ src/functions.py
@@ -6,35 +6,38 @@ class wiki:
     def __init__(self, word, lang="en") -> None:
         self.lang = lang
         self.word = word
+    
+    def alternative(self) -> bool:
+        alpha, ALPHA, comb = list("abcdefghijklmnopqrstuvwxyz"), list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), []
         
+        for x in ALPHA:
+            for y in alpha:
+                comb.append(x+y)
+        
+        if (self.word[0].upper()+self.word[1].lower()) not in comb:
+            return f"Sorry the term \"{self.word}\" is not in Wikipedia\nPlease kindly correct the term and try again."
+        
+        else:
+            wiki = "https://en.wikipedia.org/wiki/Special:AllPages/" + self.word[0].upper() + self.word[1].lower()
+            
+            if urllib.request.urlopen(wiki).getcode() not in [x for x in range(200, 299)]:
+                return f"The source is unfortunately down."
+
+            else:
+                lang = suggest.Dict("en_US")
+
+                lang.check(self.word)
+                return lang.suggest(self.word) if lang.suggest(self.word) != [] else "There is no available suggestion for the given word."
+    
     def fetch_summary(self) -> str or list:
         wiki = wikiapi.Wikipedia(self.lang)
         page_py = wiki.page(self.word)        
-        alpha, ALPHA = list("abcdefghijklmnopqrstuvwxyz"), list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-        comb = []
         
-        for x in alpha:
-            for y in alpha:
-                comb.append(x+y)
-
         if page_py.exists() is True:
             return f"{str(page_py.summary)}", page_py.fullurl, page_py.canonicalurl
 
         else:
-            if (self.word[0].upper()+self.word[1].lower()) not in comb:
-                return f"Sorry the term \"{self.word}\" is not in Wikipedia\nPlease kindly correct the term and try again."
-            
-            else:
-                wiki = "https://en.wikipedia.org/wiki/Special:AllPages/" + self.word[0].upper() + self.word[1].lower()
-                
-                if urllib.request.urlopen(wiki).getcode() not in [x for x in range(200, 299)]:
-                    return f"The source is unfortunately down."
-
-                else:
-                    lang = suggest.Dict("en_US")
-
-                    lang.check(self.word)
-                    return lang.suggest(self.word)
+            return self.alternative()
 
     def fetch_full(self) -> str:
         wiki = wikiapi.Wikipedia(self.lang, extract_format=wikiapi.ExtractFormat.WIKI)
