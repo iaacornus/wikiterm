220a84ab27c6c00a2d592800831eb2a7ce21d00e
iaacornus <iaacornus@gmail.com>
2022-04-21 19:24:31 +0800

    add section titles, and page title in return

--- src/functions.py
+++ src/functions.py
@@ -31,12 +31,12 @@ class wiki:
                 return lang.suggest(self.word) if lang.suggest(self.word) != [] else "There is no available suggestion for the given word."
     
     def fetch_info(self) -> str or list:
-        wiki = wikiapi.Wikipedia(self.lang) if self.fetch == "summary" else wikiapi.Wikipedia(self.lang, extract_format=wikiapi.ExtractFormat.WIKI)    
+        wiki = wikiapi.Wikipedia(self.lang) if self.fetch == "summary" else wikiapi.Wikipedia(language=self.lang, extract_format=wikiapi.ExtractFormat.HTML)
         page_py = wiki.page(self.word)
-
+   
         
         if page_py.exists() is True:
-            return True, f"{str(page_py.summary) if self.fetch == 'summary' else str(page_py.text)}", page_py.fullurl, page_py.canonicalurl
+            return True, page_py.title, [section.title for section in page_py.sections], f"{page_py.summary if self.fetch == 'summary' else page_py.text}", page_py.fullurl, page_py.canonicalurl
 
         else:
             return False, self.alternative()
