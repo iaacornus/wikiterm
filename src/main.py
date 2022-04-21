7e3409d82a562c2ba5bd79a09b965180f140de88
iaacornus <iaacornus@gmail.com>
2022-04-21 16:25:05 +0800

    implemented the suggestion function

--- src/main.py
+++ src/main.py
@@ -9,32 +9,32 @@ if __name__ != "__main__":
 color = colors
 
 def main(word, fetch="summary"):
-    WIKI = wiki(word.lower())
     print(f"{colors.CGREENBG}{colors.CBOLD}Fetching definition of {word.upper()} {colors.CEND}", end="\r")
-        
-    if fetch == "summary":        
-        results = WIKI.fetch_summary()
-        summary = results[0].replace("\n", "\n\n")
 
-    elif fetch == "full":
-        results = WIKI.fetch_full()
+    WIKI = wiki(word.lower())
+    results = WIKI.fetch_summary() if fetch == "summary" else WIKI.fetch_full()
 
     # remove the status 
     sys.stdout.write("\033[K")
-
-    # print the word
-    print(f"{colors.CBLUEBG}{colors.CBOLD} {word.upper()} {colors.CEND}\n")
     
-    # print the fetched information
-    print(f"{summary}\n\n{colors.CBEIGEBG}{colors.CBOLD}Wikipedia page URL(s):{colors.CEND}\n{colors.CBOLD}Full URL:{colors.CEND} {results[1]}\n{colors.CBOLD}Canonical URL:{colors.CEND} {results[2]}{colors.CEND}")
+    if results[0] is False:
+        if str(type(results[1])) == "<class 'list'>":
+            print(f"{colors.CYELLOWBG}{colors.CBOLD}Perhaps you mean:{colors.CEND}")
+            for num, words in enumerate(results[1]):
+                print(f"\t{num+1}) {words}")
+        else:
+            print(f"{colors.CREDBG}{colors.CBOLD}{results[1]}{colors.CEND}")
+    elif results[1] is True:
+        # print the word
+        print(f"{colors.CBLUEBG}{colors.CBOLD} {word.upper()} {colors.CEND}\n")
+    
+        if fetch == "summary": 
+            summary = results[1].replace("\n", "\n\n")
+            
+            # print the fetched information
+            print(f"{summary}\n\n{colors.CBEIGEBG}{colors.CBOLD}Wikipedia page URL(s):{colors.CEND}\n{colors.CBOLD}Full URL:{colors.CEND} {results[1]}\n{colors.CBOLD}Canonical URL:{colors.CEND} {results[2]}{colors.CEND}")
 
-word = "Python"
-WIKI = wiki(word.lower())
-alt = WIKI.alternative()
-
-if str(type(alt)) == "<class 'list'>":
-    print(f"{colors.CYELLOWBG}{colors.CBOLD}Perhaps you mean:{colors.CEND}")
-    for num, words in enumerate(alt):
-        print(f"\t{num+1}) {words}")
-else:
-    print(f"{colors.CREDBG}{colors.CBOLD}{alt}{colors.CEND}")
+        elif fetch == "full":
+            pass
+  
+main(word="hello")
