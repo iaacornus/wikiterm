999769e392fdf916bbf193aaf3fabdecea6f81fb
iaacornus <iaacornus@gmail.com>
2022-04-21 20:40:24 +0800

    changed from HTML into wiki, and made the return for the text contents separate

--- src/functions.py
+++ src/functions.py
@@ -31,11 +31,11 @@ class wiki:
                 return lang.suggest(self.word) if lang.suggest(self.word) != [] else "There is no available suggestion for the given word."
     
     def fetch_info(self) -> str or list:
-        wiki = wikiapi.Wikipedia(self.lang) if self.fetch == "summary" else wikiapi.Wikipedia(language=self.lang, extract_format=wikiapi.ExtractFormat.HTML)
+        wiki = wikiapi.Wikipedia(self.lang) if self.fetch == "summary" else wikiapi.Wikipedia(language=self.lang, extract_format=wikiapi.ExtractFormat.WIKI)
         page_py = wiki.page(self.word)
         
         if page_py.exists() is True:
-            return True, page_py.title, [section.title for section in page_py.sections], f"{page_py.summary if self.fetch == 'summary' else page_py.text}", page_py.fullurl, page_py.canonicalurl
+            return True, page_py.title, [section for section in page_py.sections], page_py.summary, page_py.text, page_py.fullurl, page_py.canonicalurl
 
         else:
             return False, self.alternative()
